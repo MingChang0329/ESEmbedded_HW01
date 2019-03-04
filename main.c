28cf92361811dceca86c97edb71b02a4a1978aee
Ming <john00850329@gmail.com>
2019-03-03 21:59:40 -0800

    simplize

--- main.c
+++ main.c
@@ -5,19 +5,23 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+
+typedef int (*FunPTR)(int a,int b);
+
 int main()
 {
 	int a, c;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	char* info[4]={"+","-","*","/"};
+        FunPTR P[4]={plus,minus,multiply,divided};
+        for(int i=0;i<4;i++)
+        {
+                if(b==*info[i])
+                {
+                        printf("%d %c %d = %d\n",a,b,c,P[i](a,c));
+                }
+        }
+
 }
